0562b1797b0d7c8d7b484fd90ae0f8a09fc4ef6e
Gaiokane <qk632773809@vip.qq.com>
2020-10-30 23:21:35 +0800

    测试请求

--- RegularlyExecuteHTTPRequests/Form1.Designer.cs
+++ RegularlyExecuteHTTPRequests/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_DELETE = new System.Windows.Forms.Button();
+            this.btn_PUT = new System.Windows.Forms.Button();
+            this.btn_POST = new System.Windows.Forms.Button();
+            this.btn_GET = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_DELETE
+            // 
+            this.btn_DELETE.Location = new System.Drawing.Point(365, 149);
+            this.btn_DELETE.Name = "btn_DELETE";
+            this.btn_DELETE.Size = new System.Drawing.Size(75, 23);
+            this.btn_DELETE.TabIndex = 0;
+            this.btn_DELETE.Text = "DELETE";
+            this.btn_DELETE.UseVisualStyleBackColor = true;
+            this.btn_DELETE.Click += new System.EventHandler(this.btn_DELETE_Click);
+            // 
+            // btn_PUT
+            // 
+            this.btn_PUT.Location = new System.Drawing.Point(365, 178);
+            this.btn_PUT.Name = "btn_PUT";
+            this.btn_PUT.Size = new System.Drawing.Size(75, 23);
+            this.btn_PUT.TabIndex = 1;
+            this.btn_PUT.Text = "PUT";
+            this.btn_PUT.UseVisualStyleBackColor = true;
+            this.btn_PUT.Click += new System.EventHandler(this.btn_PUT_Click);
+            // 
+            // btn_POST
+            // 
+            this.btn_POST.Location = new System.Drawing.Point(365, 207);
+            this.btn_POST.Name = "btn_POST";
+            this.btn_POST.Size = new System.Drawing.Size(75, 23);
+            this.btn_POST.TabIndex = 2;
+            this.btn_POST.Text = "POST";
+            this.btn_POST.UseVisualStyleBackColor = true;
+            this.btn_POST.Click += new System.EventHandler(this.btn_POST_Click);
+            // 
+            // btn_GET
+            // 
+            this.btn_GET.Location = new System.Drawing.Point(365, 236);
+            this.btn_GET.Name = "btn_GET";
+            this.btn_GET.Size = new System.Drawing.Size(75, 23);
+            this.btn_GET.TabIndex = 3;
+            this.btn_GET.Text = "GET";
+            this.btn_GET.UseVisualStyleBackColor = true;
+            this.btn_GET.Click += new System.EventHandler(this.btn_GET_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_GET);
+            this.Controls.Add(this.btn_POST);
+            this.Controls.Add(this.btn_PUT);
+            this.Controls.Add(this.btn_DELETE);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_DELETE;
+        private System.Windows.Forms.Button btn_PUT;
+        private System.Windows.Forms.Button btn_POST;
+        private System.Windows.Forms.Button btn_GET;
     }
 }
 
--- RegularlyExecuteHTTPRequests/Form1.cs
+++ RegularlyExecuteHTTPRequests/Form1.cs
@@ -16,5 +16,29 @@ namespace RegularlyExecuteHTTPRequests
         {
             InitializeComponent();
         }
+
+        private void btn_DELETE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_PUT_Click(object sender, EventArgs e)
+        {
+            string serviceUrl = "http://localhost/";
+            HTTPHelper httphelper = new HTTPHelper(serviceUrl);
+            string data = @"{""UserName"":""1111"",""Age"":123,""Id"":133}";
+            string uriPost = "api/values/3";
+            string retPost = httphelper.Put(data, uriPost);
+        }
+
+        private void btn_POST_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_GET_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(HTTPHelper.Get("https://www.baidu.com/s?ie=utf-8&mod=1&isbd=1&isid=BB39D62DFB618099&ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=RestClient&oq=Rest%2526lt%253Blient&rsv_pq=b9fbf0f70005442f&rsv_t=c488k%2BX1FLp26DkB2hECfqAkSnV4FLK9E7FdQKb3VoI%2Bwrw66RSWTKo9%2BSY&rqlang=cn&rsv_enter=0&rsv_dl=tb&rsv_btype=t&bs=RestClient&rsv_sid=undefined&_ss=1&clist=&hsug=&f4s=1&csor=0&_cr1=26925"));
+        }
     }
 }
